cb6b3ab0a8ed601b9778fb3cfaa3674fa3687838
bhanuprasad9111 <bhanuprasadchintu.170403@gmail.com>
2026-01-14 10:50:28 +0530

    Day 1

--- day1/Assignment.py
+++ day1/Assignment.py
@@ -1,9 +1,12 @@
+# Define the source data first so all questions can access it
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 # question 1
-for i in range(1,21):
+for i in range(1, 21):
     print(i)
 
-# question 2
-even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+# question 2 - Changed 'numbers' to 'data'
+even_numbers = list(filter(lambda x: x % 2 == 0, data))
 print(even_numbers)
 
 # question 3
@@ -11,8 +14,6 @@ squared_numbers = list(map(lambda x: x * x, even_numbers))
 print(squared_numbers)
 
 # question 4
-data = [1,2,3,4,5,6,7,8,9,10]
-
 squares = [x * x for x in data]
 print(squares)
 
